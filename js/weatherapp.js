f8958c75072e6d90f04c8c2f0589a4b9fbc7c4ae
luke munnich <lukemunnich@gmail.com>
2019-07-05 14:56:26 +0200

    added commenting

--- js/weatherapp.js
+++ js/weatherapp.js
@@ -1,19 +1,20 @@
 
 
-function myFunction(){
+function displayclothing(){
 var temperature = document.getElementById("temp").value;
 // setting a temp variable//
 
-//setting condition of the temp//
-if (temperature >=8 && temperature <=24){ 
-    document.getElementById("wear").innerHTML = "its a warmish day but not too hot wear jeans and short sleeves.";
-} else if (temperature >=25 && temperature <=50){ 
-    document.getElementById("wear").innerhtml = "stay hydrated and wear sunblock.";
-} else if (temperature >=0 && temperature <=7){ 
+//setting temperature range between 16 and 35//
+if (temperature >=16 && temperature <=35){ 
+    document.getElementById("wear").innerHTML = "the sun is shinning and its perfect for short sleeves and pants.";
+//setting temperature range between 0 and 15//
+} else if (temperature >=0 && temperature <=15){ 
     document.getElementById("wear").innerHTML = "its abit cold wear a jersey and scarve.";
+   //setting temperature range between -1 and -15// 
 } else if (temperature >=-15 && temperature <=-1){ 
     document.getElementById("wear").innerHTML = "its very cold wear your warmest clothing.";
-} else if (temperature <=-50 && temperature <=-16){ 
+    //setting temperature range between -16 and below//
+} else if (temperature <=-16){ 
     document.getElementById("wear").innerHTML = "stay indoors under a warm blanket.";
 }  else{ 
     alert("not a valid temperature.");
